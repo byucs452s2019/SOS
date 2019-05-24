42c662c4a4fc2e51bbe1538664f561825194e4ca
Neal Benner <nbenner@youniqueproducts.com>
2019-05-23 22:07:48 -0600

    additional endpoint and start to DB models

--- server/index.js
+++ server/index.js
@@ -1,6 +1,42 @@
 const express = require('express');
 const http = require('http');
+const Sequelize = require('sequelize');
 const app = express();
+
+const DB = new Sequelize({dialect: 'sqlite', storage: './db.sqlite'});
+
+class User extends Sequelize.Model {}
+User.init({
+  id: {type: Sequelize.INTEGER, primaryKey: true},
+  username: {type: Sequelize.STRING, allowNull: false},
+  password: {type: Sequelize.STRING, allowNull: false},
+}, {sequelize: DB, modelName: 'user'});
+
+class Listing extends Sequelize.Model {}
+Listing.init({
+  id: {type: Sequelize.INTEGER, primaryKey: true},
+  pic: {type: Sequelize.STRING},
+  title: {type: Sequelize.STRING, allowNull: false},
+  description: {type: Sequelize.STRING, allowNull: false},
+  user_id: {type: Sequelize.INTEGER.UNSIGNED},
+}, {sequelize: DB, modelName: 'listing'});
+
+app.get('/workers', (req, res) => {
+  return res.send([
+    {
+      id: 1,
+      user_id: 2,
+      reviews: [],
+      folios: [],
+    },
+    {
+      id: 2,
+      user_id: 3,
+      reviews: [],
+      folios: [],
+    }
+  ]);
+});
 
 app.get('/listings', (req, res) => {
   return res.send([
@@ -26,7 +62,8 @@ app.get('/listings', (req, res) => {
 });
 
 app.post('/listing', (req, res) => {
-  const {pic, title, description, user_id} = req.query;
+  const {pic, title, description, user_id} = req.body;
+
   //TODO save these to something
   return res.send(true);
 });
